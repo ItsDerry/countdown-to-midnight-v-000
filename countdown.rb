3a50fc7d517a1dd4beef00f0a26876cd66d92782
Derrick Reyes <derrickreyes@me.com>
2018-08-20 19:01:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
-#write your code here
+nye = 10
 
-def countdown
+def countdown (nye)
+  while nye > 0
+    puts "#{number} SECOND(S)!"
+    nye -= 1
+  else
+    puts "HAPPY NEW YEAR!"
+  end
 end
